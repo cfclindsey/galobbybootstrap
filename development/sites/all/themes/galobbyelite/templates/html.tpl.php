5aa7d369f0250b214e1d0fa1feeace7539147711
cfclindsey <lindsey@countryfriedcreative.com>
2019-08-09 12:31:06 -0400

    Template change 3

--- development/sites/all/themes/galobbyelite/templates/html.tpl.php
+++ development/sites/all/themes/galobbyelite/templates/html.tpl.php
@@ -4,37 +4,36 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    // Tell the browser to be responsive to screen width //
+  <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    // Favicon icon //
     <link rel="icon" type="image/png" sizes="16x16" href="https://new.galobby.com/development/sites/all/themes/galobbyelite/assets/images/favicon.png">
     <title>Georgia Lobby - In Assistance to the Lobbyist</title>
-    // This page CSS //
-    // chartist CSS //
+    <!-- This page CSS -->
+    <!-- chartist CSS -->
     <link href="https://new.galobby.com/development/sites/all/themes/galobbyelite/assets/node_modules/morrisjs/morris.css" rel="stylesheet">
-    //Toaster Popup message CSS //
+    <!--Toaster Popup message CSS -->
     <link href="https://new.galobby.com/development/sites/all/themes/galobbyelite/assets/node_modules/toast-master/css/jquery.toast.css" rel="stylesheet">
-    // Custom CSS //
+    <!-- Custom CSS -->
     <link href="https://new.galobby.com/development/sites/all/themes/galobbyelite/dist/css/style.min.css" rel="stylesheet">
-    // Dashboard 1 Page CSS //
-    <link href="https://new.galobby.com/development/sites/all/themes/galobbyelite/dist/css/pages/dashboard1.css" rel="stylesheet">
-    <link href="https://new.galobby.com/development/sites/all/themes/galobbyelite/assets/node_modules/bootstrap-table/dist/bootstrap-table.min.css" rel="stylesheet" type="text/css" />
-    // Custom CSS //
+    <!-- Dashboard 1 Page CSS -->
+  <link href="https://new.galobby.com/development/sites/all/themes/galobbyelite/dist/css/pages/dashboard1.css" rel="stylesheet">
+    <!--link href="https://new.galobby.com/development/sites/all/themes/galobbyelite/assets/node_modules/bootstrap-table/dist/bootstrap-table.min.css" rel="stylesheet" type="text/css" />
+    <!-- Custom CSS -->
     <link href="https://new.galobby.com/development/sites/all/themes/galobbyelite/dist/css/custom.css" rel="stylesheet">
-    // HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries //
-    // WARNING: Respond.js doesn't work if you view the page via file:// //
-    //[if lt IE 9]>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]//
+<![endif]-->
 </head>
 
 <body class="skin-default fixed-layout">
-    // ============================================================== //
-    // Preloader - style you can find in spinners.css //
-    // ============================================================== //
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
     <div class="preloader">
         <div class="loader">
             <div class="loader__figure"></div>
@@ -1533,44 +1532,41 @@
     // End Wrapper //
     // ============================================================== //
     */ ?>
-    // ============================================================== //
-    // All Jquery //
-    // ============================================================== //
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+  <!-- ============================================================== -->
     <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/assets/node_modules/jquery/jquery-3.2.1.min.js"></script>
-    // Bootstrap popper Core JavaScript //
+    <!-- Bootstrap popper Core JavaScript -->
     <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/assets/node_modules/popper/popper.min.js"></script>
     <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/assets/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-    // slimscrollbar scrollbar JavaScript //
+    <!--slimscrollbar scrollbar JavaScript -->
     <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/dist/js/perfect-scrollbar.jquery.min.js"></script>
-    //Wave Effects //
     <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/dist/js/waves.js"></script>
-    //Menu sidebar //
+    <!--Menu sidebar -->
     <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/dist/js/sidebarmenu.js"></script>
-    //Custom JavaScript //
+    <!--Custom JavaScript-->
     <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/dist/js/custom.min.js"></script>
-    // ============================================================== //
+    <!-- ============================================================== //
     // This page plugins //
     // ============================================================== //
-    //morris JavaScript //
+    //morris JavaScript -->
     <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/assets/node_modules/raphael/raphael-min.js"></script>
     <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/assets/node_modules/morrisjs/morris.min.js"></script>
     <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/assets/node_modules/jquery-sparkline/jquery.sparkline.min.js"></script>
-    // Popup message jquery
-    <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/assets/node_modules/toast-master/js/jquery.toast.js"></script> //
-    // Chart JS //
-    <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/dist/js/dashboard1.js"></script>
-    // Popup message jquery //
+    <!--Popup message jquery -->
     <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/assets/node_modules/toast-master/js/jquery.toast.js"></script>
 
-    //Custom JavaScript
-    <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/dist/js/custom.min.js"></script> //
+    <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/dist/js/dashboard1.js"></script>
+
+    <!--Custom JavaScript -->
+    <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/dist/js/custom.min.js"></script>
     <script src="https://unpkg.com/tableexport.jquery.plugin/tableExport.min.js"></script>
     <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/assets/node_modules/bootstrap-table/dist/bootstrap-table.min.js"></script>
     <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/assets/node_modules/bootstrap-table/dist/bootstrap-table-locale-all.min.js"></script>
     <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/assets/node_modules/bootstrap-table/dist/extensions/export/bootstrap-table-export.min.js"></script>
     <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/dist/js/pages/bootstrap-table.init.js"></script>
 
-    // wysuhtml5 Plugin JavaScript //
+    <!--  wysuhtml5 Plugin JavaScript -->
     <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/assets/node_modules/tinymce/tinymce.min.js"></script>
     <script>
     $(function() {
@@ -1594,7 +1590,8 @@
 
     <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/assets/node_modules/datatables.net/js/jquery.dataTables.min.js"></script>
     <script src="https://new.galobby.com/development/sites/all/themes/galobbyelite/assets/node_modules/datatables.net-bs4/js/dataTables.responsive.min.js"></script>
-    // start - This is for export functionality only //
+
+    <!-- start - This is for export functionality -->
     <script src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
@@ -1602,7 +1599,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
     <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.print.min.js"></script>
-    // end - This is for export functionality only //
+    
     <script>
         $(function () {
             $('#myTable').DataTable();
